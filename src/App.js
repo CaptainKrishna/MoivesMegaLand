4ee8128abeadedc0344a2b4678648fd87e603de9
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-07-05 22:44:46 +0530

    First MoviesReactApp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
-
+import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
 const API_URL = "http://www.omdbapi.com?apikey=f28b8592";
 
@@ -16,6 +15,7 @@ const App = () => {
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
+
     const data = await response.json();
 
     setMovies(data.Search);
